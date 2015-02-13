3243d68ff151a45e65f2254b9c42cc13b46f696d
Jenia Tretiyakova <zhanyat@gmail.com>
2015-02-13 18:09:16 -0500

    Validated the date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,10 @@ belongs_to :user
 belongs_to :restaurant
 validates :time, numericality: {only_integer: true, greater_than: 11, less_than: 22}
 validates :party_size, numericality: {only_integer: true, greater_than: 2, less_than: 20}
+validate :date_must_be_between
+
+	def date_must_be_between
+		errors.add(:date, "Date is too far away. Only book one month ahead.") unless date < Date.today + 1.month
+	end
+
 end
